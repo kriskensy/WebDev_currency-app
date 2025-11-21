e892c924c9d01ce9fe04bc9723faae10df93b790
kris.kensy <kriskensy@proton.me>
2025-11-21 21:57:48 +0100

    Add conversion PLN->USD test do ResultBox

--- src/components/ResultBox/ResultBox.test.js
+++ src/components/ResultBox/ResultBox.test.js
@@ -1,8 +1,14 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import '@testing-library/jest-dom/extend-expect';
 import ResultBox from "./ResultBox";
 
 describe('Component ResultBox', () => {
   it('should render without crashing', () => {
-    render(<ResultBox amount={56} from="PLN" to="USD"/>)
+    render(<ResultBox amount={35} from="PLN" to="USD"/>)
+  });
+  it('should render proper info about conversion PLN -> USD', () => {
+    render(<ResultBox from="PLN" to="USD" amount={35}/>);
+    const conversionOutput = screen.getByTestId('conversionOutput');
+    expect(conversionOutput).toHaveTextContent('PLN 35.00 = $10.00');
   });
 });
